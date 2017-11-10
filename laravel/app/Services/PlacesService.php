e72677d28bce2305b193daef9b12bcac6ebf6634
Ivan Petrushev <ivan.petrushev@dnhsoft.com>
2017-11-10 11:54:03 +0200

    import: parsing descriptions and site titles

--- laravel/app/Services/PlacesService.php
+++ laravel/app/Services/PlacesService.php
@@ -11,6 +11,8 @@ class PlacesService {
     protected $iconVisited = '#icon-61';
     protected $iconNotVisited = '#icon-22';
 
+    protected $titleMap = [];
+
     public function import()
     {
         $sKmlSource = file_get_contents('https://www.google.com/maps/d/kml?forcekml=1&mid=' . $this->mid . '&nc=' . time());
@@ -38,9 +40,15 @@ class PlacesService {
             $oRecord->is_visited = (bool) (strstr($item['styleUrl'], $this->iconVisited));
             $oRecord->category_id = null;
 
-            $description = strip_tags($item['description']);
+            $description = preg_replace('/\<br(\s*)?\/?\>/i', "\n", $item['description']);
+            $description = strip_tags($description);
+            preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $description, $matches);
+            foreach ($matches[0] as $match) {
+                $description = str_replace($match, '', $description);
+                $urls[] = $match;
+            }
 
-            $oRecord->description = $description;
+            $oRecord->description = trim($description);
 
             $coordinates = trim($item['Point']['coordinates']);
             $coordinates = explode(',', $coordinates);
@@ -56,10 +64,51 @@ class PlacesService {
                 $oRecordLink = new PlaceLink();
                 $oRecordLink->place_id = $oRecord->id;
                 $oRecordLink->url = $url;
-                $oRecordLink->title = 'image';
+
+                if (stristr($url,'googleusercontent')) {
+                    $oRecordLink->title = 'image';
+                } elseif (stristr($url, 'blog.ivanatora.info')) {
+                    $oRecordLink->title = $this->getTitle($url);
+                } else {
+                    $oRecordLink->title = $url;
+                }
+
+                if (stristr($url, 'blog.ivanatora.info')) {
+                    $oRecordLink->is_blog = 1;
+                }
+
                 $oRecordLink->save();
             }
         }
         print_r($data);
+    }
+
+    public function getTitle($url)
+    {
+        $filenameMap = 'url-title-map.txt';
+
+        if (empty($this->titleMap) && file_exists($filenameMap)) {
+            $mapSerialized = file_get_contents($filenameMap);
+            if (! empty($mapSerialized)) {
+                $this->titleMap = unserialize($mapSerialized);
+            }
+        }
+
+        if (isset($this->titleMap[$url])) {
+            return $this->titleMap[$url];
+        }
+        $str = file_get_contents($url);
+
+        print "GET TITLE: $url\n";
+
+        if (strlen($str) > 0) {
+            $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+            preg_match("/\<title\>(.*)\<\/title\>/i", $str, $title); // ignore case
+            $this->titleMap[$url] = $title[1];
+
+            file_put_contents($filenameMap, serialize($this->titleMap));
+
+            return $title[1];
+        }
     }
 }
