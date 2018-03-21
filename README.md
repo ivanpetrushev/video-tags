71acee265134033e98e74327672763f5b280cc3d
ivanatora <ivanatora@gmail.com>
2018-03-21 20:13:06 +0200

    initial db migrations

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## Install
 
 * Checkout the repository in your local directory
-* `docker-compose up` will build and start containers
-* `docker exec -it videotags_php_1 bash`
-* `# composer.phar install`
+* This project is using `apache`, `mysql` and `phpmyadmin`. If you have other apps using those services, edit `docker-compose.yml` and change port numbers
+* `$ docker-compose up` will build and start containers
+* `$ docker exec -it videotags_php_1 composer.phar install`
 
 
 This projects come with preinstalled Laravel 5.4. If you want to wipe it, just remove everything in `laravel/`
---
+++ laravel/database/migrations/2018_03_21_180214_create_base_tables.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBaseTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('path');
+        });
+
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('path');
+            $table->string('filename', 255)->default('');
+            $table->integer('duration')->default(0);
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('');
+        });
+
+        Schema::create('files_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('file_id')->nullable();
+            $table->unsignedInteger('tag_id')->nullable();
+            $table->integer('start_time')->default(0);
+            $table->integer('duration')->default(0);
+
+            $table->index('file_id');
+            $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
+
+            $table->index('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files_tags');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('files');
+        Schema::dropIfExists('directories');
+    }
+}
